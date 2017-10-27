7dda7f49155ea5010867e9c040aa2b98dc8c2db2
gagan0183 <g.deepsingh1@gmail.com>
2017-10-27 20:50:52 +0530

    creating componentp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { BooksComponent } from './books/books.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookItemComponent } from './books/book-list/book-item.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BooksComponent,
+    BookListComponent,
+    BookItemComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
