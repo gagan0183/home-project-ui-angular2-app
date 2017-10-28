145c63c0fc09bc2797064279fc9837645a5bb1f6
gagan0183 <g.deepsingh1@gmail.com>
2017-10-28 19:53:19 +0530

    creating headers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BooksComponent } from './books/books.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { BookItemComponent } from './books/book-list/book-item.component';
 import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { DropdownDirective } from './dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { BookDetailComponent } from './books/book-detail/book-detail.component';
     BooksComponent,
     BookListComponent,
     BookItemComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
